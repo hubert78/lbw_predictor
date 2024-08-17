471be61ebb3873dc459ebe8a0bcfcc45b03d09af
Hubert Owusu <hubert7856@gmail.com>
2024-08-17 14:22:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,26 @@ col1, col2 = st.columns(2)
 with col1:
   age = st.number_input('Age')
 with col2:
-  levelofeducation = st.selectbox('Level of education', ['Secondary', 'Illiterate', 'Tertiary', 'Basic'])
+  levelofeducation = st.selectbox('Level of education', index= 'Tertiary' ['Basic', 'Illiterate', 'Secondary', 'Tertiary'])
 
 col1, col2 = st.columns(2)
 with col1:
-  occupation = st.selectbox('Occupation', ['Self employed', 'Unemployed', 'Civil Servant', 'Other'])
+  occupation = st.selectbox('Occupation', ['Civil Servant', 'Self employed', 'Unemployed', 'Other'])
 with col2:
-  occupation = st.selectbox('Occupation', ['B Pos', 'O Pos', 'O Neg', 'A Pos', 'A Neg', 'AB Pos', 'B Neg', 'AB Neg'])
+  occupation = st.selectbox('Blood Type', ['A Neg', 'A Pos', 'AB Neg', 'AB Pos', 'B Neg', 'B Pos', 'O Neg', 'O Pos'])
+
+col1, col2 = st.columns(2)
+with col1:
+  occupation = st.number_input('Gravidity')
+with col2:
+  occupation = st.number_input('Parity')
+
+col1, col2 = st.columns(2)
+with col1:
+  occupation = st.number_input('Gravidity')
+with col2:
+  occupation = st.number_input('Parity')
+
+
+
+
