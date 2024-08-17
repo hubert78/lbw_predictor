f7c0c88ef938a292b0771594740226b1ac99124b
Hubert Owusu <hubert7856@gmail.com>
2024-08-17 14:55:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,32 +8,45 @@ st.title('Low Birth Weight Predictor')
 levelofeducation_options = ['Basic', 'Illiterate', 'Secondary', 'Tertiary']
 occupation_options = ['Civil Servant', 'Self employed', 'Unemployed', 'Other']
 bloodgroup = ['A Neg', 'A Pos', 'AB Neg', 'AB Pos', 'B Neg', 'B Pos', 'O Neg', 'O Pos']
+serology_test = ['Non Reactive', 'Reactive']
 
+
+st.write('Patient socio-demographic details')
+col1, col2 = st.columns(2)
+with col1:
+  maternal_age = st.number_input('Maternal age', min_value=10, max_value=50, value=26)
+with col2:
+  levelofeducation = st.selectbox('Level of education', levelofeducation_options, index=levelofeducation_options.index('Illiterate'))
 
 col1, col2 = st.columns(2)
 with col1:
-  age = st.number_input('Age')
+  occupation = st.selectbox('Occupation', occupation_options, index=occupation_options.index('Self employed'))
+
+
+
+st.write('Pregnancy history')
+col1, col2 = st.columns(2)
+with col1:
+  occupation = st.number_input('Gravidity', min_value=1, max_value=10, value=1)
 with col2:
-  levelofeducation = st.selectbox('Level of education', levelofeducation_options, index=levelofeducation_options.index('Basic'))
+  occupation = st.number_input('Parity', min_value=0, max_value=10, value=0  )
 
 col1, col2 = st.columns(2)
 with col1:
-  occupation = st.selectbox('Occupation', occupation_options, index=occupation_options.index('Civil Servant'))
+  antenatal_visits = st.number_input('Number of antenatal visits', min_value=0, max_value=20, value=7)
 with col2:
+  gestational_age = st.number_input('Gestational age', min_value=20, max_value=43, value=38)
+
+
+st.write('Laboratory results')
+col1, col2 = st.columns(2)
+with col1:
   bloodgroup = st.selectbox('Blood Type', bloodgroup, index=bloodgroup.index('O Pos'))
-
+with col2:
+  hepatitis = st.selectbox('Hepatitis B Status', serology_test, index=serology_test.index('Non Reactive'))
+  
 col1, col2 = st.columns(2)
 with col1:
-  occupation = st.number_input('Gravidity')
+  retro = st.selectbox('Retro (HIV) Status', serology_test, index=serology_test.index('Non Reactive'))
 with col2:
-  occupation = st.number_input('Parity')
-
-col1, col2 = st.columns(2)
-with col1:
-  occupation = st.number_input('Number of antenatal visits', min_value=0, max_value=30, value=10)
-with col2:
-  occupation = st.number_input('Gestational age', min_value=20, max_value=43, value=37)
-
-
-
-
+  syphillis = st.selectbox('Syphillis Status', serology_test, index=serology_test.index('Non Reactive'))
