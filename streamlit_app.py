db2565ebb9406f92da96ab05b18c5c242c3b86ae
Hubert Owusu <hubert7856@gmail.com>
2024-08-17 14:25:09 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,11 @@ col1, col2 = st.columns(2)
 with col1:
   age = st.number_input('Age')
 with col2:
-  levelofeducation = st.selectbox('Level of education', ['Basic', 'Illiterate', 'Secondary', 'Tertiary'], index= 'Tertiary')
+  levelofeducation = st.selectbox('Level of education', ['Basic', 'Illiterate', 'Secondary', 'Tertiary'])
 
 col1, col2 = st.columns(2)
 with col1:
-  occupation = st.selectbox('Occupation', ['Civil Servant', 'Self employed', 'Unemployed', 'Other'])
+  occupation = st.selectbox('Occupation', options, index=options.index(default_value))
 with col2:
   occupation = st.selectbox('Blood Type', ['A Neg', 'A Pos', 'AB Neg', 'AB Pos', 'B Neg', 'B Pos', 'O Neg', 'O Pos'])
 
