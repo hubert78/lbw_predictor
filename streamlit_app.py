e4fd4a9f6e367328b5240b2761ae5b52f5008fe8
Hubert Owusu <hubert7856@gmail.com>
2024-08-17 14:33:15 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,22 @@ import numpy as np
 
 st.title('Low Birth Weight Predictor')
 
-options = ['Civil Servant', 'Self employed', 'Unemployed', 'Other']
-default_value = 'Civil Servant'
+levelofeducation_options = ['Basic', 'Illiterate', 'Secondary', 'Tertiary']
+occupation_options = ['Civil Servant', 'Self employed', 'Unemployed', 'Other']
+bloodgroup = ['A Neg', 'A Pos', 'AB Neg', 'AB Pos', 'B Neg', 'B Pos', 'O Neg', 'O Pos']
+
 
 col1, col2 = st.columns(2)
 with col1:
   age = st.number_input('Age')
 with col2:
-  levelofeducation = st.selectbox('Level of education', ['Basic', 'Illiterate', 'Secondary', 'Tertiary'])
+  levelofeducation = st.selectbox('Level of education', levelofeducation_options, index=levelofeducation_options.index('Basic'))
 
 col1, col2 = st.columns(2)
 with col1:
-  occupation = st.selectbox('Occupation', options, index=options.index(default_value))
+  occupation = st.selectbox('Occupation', occupation_options, index=occupation_options.index('Civil Servant'))
 with col2:
-  occupation = st.selectbox('Blood Type', ['A Neg', 'A Pos', 'AB Neg', 'AB Pos', 'B Neg', 'B Pos', 'O Neg', 'O Pos'])
+  bloodgroup = st.selectbox('Blood Type', bloodgroup, index=bloodgroup.index('O Pos'))
 
 col1, col2 = st.columns(2)
 with col1:
