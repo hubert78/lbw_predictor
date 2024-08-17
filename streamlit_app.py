2b64fba809b115cdccf4289aa5c3f95936248990
Hubert Owusu <hubert7856@gmail.com>
2024-08-17 14:07:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,14 @@ import numpy as np
 
 st.title('Low Birth Weight Predictor')
 
-1col1, 1col2 = st.columns(2)
-with 1col1:
+col1, col2 = st.columns(2)
+with col1:
   age = st.number_input('Age')
-with 1col2:
+with col2:
   levelofeducation = st.selectbox('Level of education', ['Secondary', 'Illiterate', 'Tertiary', 'Basic'])
 
-2col1, 2col2 = st.columns(2)
-with 2col1:
+col1, col2 = st.columns(2)
+with col1:
   occupation = st.selectbox('Occupation', ['Self employed', 'Unemployed', 'Civil Servant', 'Other'])
-with 2col2:
+with col2:
   occupation = st.selectbox('Occupation', ['B Pos', 'O Pos', 'O Neg', 'A Pos', 'A Neg', 'AB Pos', 'B Neg', 'AB Neg'])
