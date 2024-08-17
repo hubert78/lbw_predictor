c0ffd180ff66c5f947e77ff01d6808ee54371911
Hubert Owusu <hubert7856@gmail.com>
2024-08-17 14:06:26 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+st.title('Low Birth Weight Predictor')
+
+1col1, 1col2 = st.columns(2)
+with 1col1:
+  age = st.number_input('Age')
+with 1col2:
+  levelofeducation = st.selectbox('Level of education', ['Secondary', 'Illiterate', 'Tertiary', 'Basic'])
+
+2col1, 2col2 = st.columns(2)
+with 2col1:
+  occupation = st.selectbox('Occupation', ['Self employed', 'Unemployed', 'Civil Servant', 'Other'])
+with 2col2:
+  occupation = st.selectbox('Occupation', ['B Pos', 'O Pos', 'O Neg', 'A Pos', 'A Neg', 'AB Pos', 'B Neg', 'AB Neg'])
