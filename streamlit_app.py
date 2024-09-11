91d3ca6ae538c7a61b970c0994fa52d6c26a465d
Hubert Owusu <hubert7856@gmail.com>
2024-09-11 21:47:04 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,29 +5,37 @@ from diabetes import diabetes_predictor
 
 
 
-st.write("")
-st.markdown("""
-    <style>
-    .centered-title {
-            display: flex; justify-content: center; align-items: center; text-align: center; margin: 0; font-size: 3em; font-weight: bold;
-            }
-    /* Responsive design for smaller screens */
-    @media (max-width: 600px) {.centered-title {font-size: 2.0em;}}
-    </style>
-
-    <div class="centered-title">MARTERNAL HEALTHCARE</div>
-""", unsafe_allow_html=True)
-
-
-condition = st.selectbox(
-    "",
-    ["Select Health Condition", "Preterm Delivery", "Low Birth Weight", "Diabetes"]
-)
+def main():
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.markdown("""
+        <style>
+        .centered-title {
+                display: flex; justify-content: center; align-items: center; text-align: center; margin: 0; font-size: 3em; font-weight: bold;
+                }
+        /* Responsive design for smaller screens */
+        @media (max-width: 600px) {.centered-title {font-size: 2.0em;}}
+        </style>
     
+        <div class="centered-title">MARTERNAL HEALTHCARE</div>
+    """, unsafe_allow_html=True)
+    
+    
+    condition = st.selectbox(
+        "",
+        ["Select Health Condition", "Preterm Delivery", "Low Birth Weight", "Diabetes"]
+    )
+        
+    
+    if condition == "Preterm Delivery":
+        ptd_predictor()
+    elif condition == "Low Birth Weight":
+        lbw_predictor()
+    elif condition == "Diabetes":
+        diabetes_predictor()
 
-if condition == "Preterm Delivery":
-    ptd_predictor()
-elif condition == "Low Birth Weight":
-    lbw_predictor()
-elif condition == "Diabetes":
-    diabetes_predictor()
+
+if __name__ == "__main__":
+    main()
